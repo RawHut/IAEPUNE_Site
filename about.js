9baa3f87d68d47d2aa66d060965d866dea69aa1d
Adit Gadkary <Adit.Gadkary@outlook.com>
2025-05-05 15:34:00 +0530

    Update about.js

--- about.js
+++ about.js
@@ -1,10 +1,4 @@
 function toggleInfo(card) {
   const text = card.querySelector(".info-text");
-  const isVisible = text.classList.contains("visible");
-
-  if (isVisible) {
-    text.classList.remove("visible");
-  } else {
-    text.classList.add("visible");
-  }
+  text.classList.toggle("visible");
 }
